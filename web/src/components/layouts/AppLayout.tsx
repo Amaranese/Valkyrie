b811e2f7e5931042b8d9a62a5aac2b193589cdc0
Amaranese <alejandromaranes@gmail.com>
2022-05-27 11:43:51 +0200

    Create AppLayout.tsx

---
+++ web/src/components/layouts/AppLayout.tsx
@@ -0,0 +1,20 @@
+import { Grid } from '@chakra-ui/react';
+import React from 'react';
+
+interface AppLayoutProps {
+  showLastColumn?: boolean | null;
+  children: React.ReactNode;
+}
+
+export const AppLayout: React.FC<AppLayoutProps> = ({ showLastColumn = false, children }) => (
+  // Col Chat: GuildList ChannelList Chat [MemberList]
+  // Col Home: GuildList DMList Chat/FriendsList
+  <Grid
+    height="100vh"
+    templateColumns={`75px 240px 1fr ${showLastColumn ? '240px' : ''} `}
+    templateRows="auto 1fr auto"
+    bg="brandGray.light"
+  >
+    {children}
+  </Grid>
+);
