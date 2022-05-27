214b0bec32f87a303353f732616084f87f0fecb4
Amaranese <alejandromaranes@gmail.com>
2022-05-27 11:39:25 +0200

    Create StyledTooltip.tsx

---
+++ web/src/components/sections/StyledTooltip.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Tooltip } from '@chakra-ui/react';
+
+type Placement = 'top' | 'right';
+
+interface StyledTooltipProps {
+  label: string;
+  position: Placement;
+  disabled?: boolean;
+  children: React.ReactNode;
+}
+
+export const StyledTooltip: React.FC<StyledTooltipProps> = ({ label, position, disabled = false, children }) => (
+  <Tooltip
+    hasArrow
+    label={label}
+    placement={position}
+    isDisabled={disabled}
+    bg="brandGray.darkest"
+    color="white"
+    fontWeight="semibold"
+    py={1}
+    px={3}
+  >
+    {children}
+  </Tooltip>
+);
