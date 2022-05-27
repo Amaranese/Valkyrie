c96f2de0b965e279907983cf3fdf36f13dcc5284
Amaranese <alejandromaranes@gmail.com>
2022-05-27 11:43:15 +0200

    Create dmScrollerCSS.ts

---
+++ web/src/components/layouts/home/css/dmScrollerCSS.ts
@@ -0,0 +1,12 @@
+export const dmScrollerCss = {
+  '&::-webkit-scrollbar': {
+    width: '4px',
+  },
+  '&::-webkit-scrollbar-track': {
+    width: '4px',
+  },
+  '&::-webkit-scrollbar-thumb': {
+    background: 'brandGray.darker',
+    borderRadius: '18px',
+  },
+};
