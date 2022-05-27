368db5136c79de1b2c3e65f70b47f1cfa93177d2
Amaranese <alejandromaranes@gmail.com>
2022-05-27 11:31:44 +0200

    Create querykeys.ts

---
+++ web/src/lib/utils/querykeys.ts
@@ -0,0 +1,9 @@
+export const gKey = 'guilds';
+export const dmKey = 'dms';
+export const aKey = 'account';
+export const fKey = 'friends';
+export const rKey = 'requests';
+export const nKey = 'notification';
+
+export const cKey = (guildId: string): string => `channels-${guildId}`;
+export const mKey = (guildId: string): string => `members-${guildId}`;
