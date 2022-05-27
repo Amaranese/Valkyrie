0bb38aed1cd50fae6042ef1ec4b1521b0612b075
Amaranese <alejandromaranes@gmail.com>
2022-05-27 11:37:33 +0200

    Create settingsStore.ts

---
+++ web/src/lib/stores/settingsStore.ts
@@ -0,0 +1,19 @@
+import create from 'zustand';
+import { persist } from 'zustand/middleware';
+
+type SettingsState = {
+  showMembers: boolean;
+  toggleShowMembers: () => void;
+};
+
+export const settingsStore = create<SettingsState>(
+  persist(
+    (set, get) => ({
+      showMembers: true,
+      toggleShowMembers: () => set({ showMembers: !get().showMembers }),
+    }),
+    {
+      name: 'settings-storage',
+    }
+  )
+);
