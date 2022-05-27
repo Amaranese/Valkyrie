28efee670809f4cc724af7d52f4ca01b25110e88
Amaranese <alejandromaranes@gmail.com>
2022-05-27 11:44:43 +0200

    Create App.tsx

---
+++ web/src/App.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { AppRoutes } from './routes/Routes';
+import { GlobalState } from './components/sections/GlobalState';
+
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: Infinity,
+      cacheTime: 0,
+    },
+  },
+});
+
+export const App: React.FC = () => (
+  <QueryClientProvider client={client}>
+    <GlobalState>
+      <AppRoutes />
+    </GlobalState>
+  </QueryClientProvider>
+);
