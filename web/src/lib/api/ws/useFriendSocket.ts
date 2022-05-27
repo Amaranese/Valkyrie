412cd8a1c8f3945fe9bdc040d0d7787b6dd1b147
Amaranese <alejandromaranes@gmail.com>
2022-05-27 11:38:00 +0200

    Create useFriendSocket.ts

---
+++ web/src/lib/api/ws/useFriendSocket.ts
@@ -0,0 +1,85 @@
+import { useEffect } from 'react';
+import { useQueryClient } from 'react-query';
+import { getSocket } from '../getSocket';
+import { userStore } from '../../stores/userStore';
+import { fKey } from '../../utils/querykeys';
+import { homeStore } from '../../stores/homeStore';
+import { Friend } from '../../models/friend';
+
+type WSMessage =
+  | { action: 'toggle_online' | 'toggle_offline' | 'remove_friend'; data: string }
+  | { action: 'requestCount'; data: number }
+  | { action: 'add_friend'; data: Friend };
+
+export function useFriendSocket(): void {
+  const current = userStore((state) => state.current);
+  const setRequests = homeStore((state) => state.setRequests);
+  const cache = useQueryClient();
+
+  useEffect((): any => {
+    const socket = getSocket();
+    socket.send(
+      JSON.stringify({
+        action: 'joinUser',
+        room: current?.id,
+      })
+    );
+    socket.send(JSON.stringify({ action: 'getRequestCount' }));
+
+    socket.addEventListener('message', (event) => {
+      const response: WSMessage = JSON.parse(event.data);
+      switch (response.action) {
+        case 'toggle_online': {
+          cache.setQueryData<Friend[]>(fKey, (d) => {
+            const data = d ?? [];
+            const index = data.findIndex((m) => m.id === response.data);
+            if (index !== -1) data[index].isOnline = true;
+            return data;
+          });
+          break;
+        }
+
+        case 'toggle_offline': {
+          cache.setQueryData<Friend[]>(fKey, (d) => {
+            const data = d ?? [];
+            const index = data.findIndex((m) => m.id === response.data);
+            if (index !== -1) data[index].isOnline = false;
+            return data;
+          });
+          break;
+        }
+
+        case 'requestCount': {
+          setRequests(response.data);
+          break;
+        }
+
+        case 'add_friend': {
+          cache.setQueryData<Friend[]>(fKey, (data) =>
+            [...data!, response.data].sort((a, b) => a.username.localeCompare(b.username))
+          );
+          break;
+        }
+
+        case 'remove_friend': {
+          cache.setQueryData<Friend[]>(fKey, (data) => [...data!.filter((m) => m.id !== response.data)]);
+          break;
+        }
+
+        default:
+          break;
+      }
+    });
+
+    return () => {
+      socket.send(
+        JSON.stringify({
+          action: 'leaveRoom',
+          room: current?.id,
+        })
+      );
+
+      socket.close();
+    };
+  }, [cache, current, setRequests]);
+}
