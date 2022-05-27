a8938e118fad671b73390788d5a37d280fb1be18
Amaranese <alejandromaranes@gmail.com>
2022-05-27 11:43:48 +0200

    Create ChannelScrollerCSS.ts

---
+++ web/src/components/layouts/guild/css/ChannelScrollerCSS.ts
@@ -0,0 +1,12 @@
+export const channelScrollbarCss = {
+  '&::-webkit-scrollbar': {
+    width: '4px',
+  },
+  '&::-webkit-scrollbar-track': {
+    width: '4px',
+  },
+  '&::-webkit-scrollbar-thumb': {
+    background: 'brandGray.darker',
+    borderRadius: '18px',
+  },
+};
