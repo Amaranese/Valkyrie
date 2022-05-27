f24db4165981ee076351c7277838a50134cf22a1
Amaranese <alejandromaranes@gmail.com>
2022-05-27 11:37:28 +0200

    Create channelStore.ts

---
+++ web/src/lib/stores/channelStore.ts
@@ -0,0 +1,15 @@
+import create from 'zustand';
+
+type ChannelState = {
+  typing: string[];
+  addTyping: (username: string) => void;
+  removeTyping: (username: string) => void;
+  reset: () => void;
+};
+
+export const channelStore = create<ChannelState>((set) => ({
+  typing: [],
+  addTyping: (username) => set((state) => ({ typing: [...state.typing, username] })),
+  removeTyping: (username) => set((state) => ({ typing: [...state.typing.filter((u) => u !== username)] })),
+  reset: () => set({ typing: [] }),
+}));
